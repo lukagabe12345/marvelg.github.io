c6b31a2a2843efcf39a20f5a01182c6888df08a1
luka gabelaia <95130225+lukagabe12345@users.noreply.github.com>
2023-07-09 19:38:25 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,251 @@
+const showe=document.getElementById("show");
+
+const videoe=document.querySelector("video");
+const videoe1=document.querySelector("#video1");
+
+showe.addEventListener("click", () => {
+    videoe.style.display="block";
+});
+   
+
+function vid1() {
+document.getElementById("vid1").style.display="block";
+}
+
+
+function vid2(){
+    document.getElementById("vid2").style.display="block";
+}
+
+function vid3() {
+    document.getElementById("thorjs1").style.display="block";
+    }
+    
+    
+    function vid4(){
+        document.getElementById("thorjs2").style.display="block";
+    }
+
+    function vid5(){
+        document.getElementById("thorjs3").style.display="block";
+    }
+
+    function vid6(){
+        document.getElementById("marveljs").style.display="block";
+    }
+
+
+    function vid7() {
+        document.getElementById("pantherjs").style.display="block";
+        }
+        
+        
+        function vid8(){
+            document.getElementById("tomspijs").style.display="block";
+        }
+        
+        function vid9() {
+            document.getElementById("tomspi2js").style.display="block";
+            }
+            
+            
+            function vid10(){
+                document.getElementById("wakanda2js").style.display="block";
+            }
+        
+            function vid11(){
+                document.getElementById("winterjs").style.display="block";
+            }
+        
+            function vid12(){
+                document.getElementById("galaxy1js").style.display="block";
+            }
+
+            function vid13(){
+                document.getElementById("galaxy2js").style.display="block";
+            }
+
+            function vid14(){
+                document.getElementById("ant1js").style.display="block";
+            }
+
+            
+            function vid15(){
+                document.getElementById("ant2js").style.display="block";
+            }
+
+            
+
+
+            function vid16() {
+                document.getElementById("strange2js").style.display="block";
+                }
+                
+                
+                function vid17(){
+                    document.getElementById("thor4js").style.display="block";
+                }
+                
+                function vid18() {
+                    document.getElementById("eternalsjs").style.display="block";
+                    }
+                    
+                    
+                    function vid19(){
+                        document.getElementById("avengers1js").style.display="block";
+                    }
+                
+                    function vid20(){
+                        document.getElementById("avengers2js").style.display="block";
+                    }
+                
+                    function vid21(){
+                        document.getElementById("avengers3js").style.display="block";
+                    }
+                
+                
+                    function vid22() {
+                        document.getElementById("avengers4js").style.display="block";
+                        }
+                        
+                        
+                        function vid23(){
+                            document.getElementById("spidermannowayhomejs").style.display="block";
+                        }
+                        
+                        function vid24() {
+                            document.getElementById("shangchi").style.display="block";
+                            }
+                            
+                            
+                            function vid25(){
+                                document.getElementById("americacap1js").style.display="block";
+                            }
+                        
+                            function vid26(){
+                                document.getElementById("strange1js").style.display="block";
+                            }
+                        
+                            function vid27(){
+                                document.getElementById("hulkjs").style.display="block";
+                            }
+                
+                            function vid28(){
+                                document.getElementById("xmen1js").style.display="block";
+                            }
+                
+                            function vid29(){
+                                document.getElementById("xmen2js").style.display="block";
+                            }
+                
+                            
+                            function vid30(){
+                                document.getElementById("xmen3js").style.display="block";
+                            }
+                
+                            
+
+
+
+                            function vid31() {
+                                document.getElementById("xmen4js").style.display="block";
+                                }
+                                
+                                
+                                function vid32(){
+                                    document.getElementById("xmen5js").style.display="block";
+                                }
+                            
+                                function vid33(){
+                                    document.getElementById("xmen6js").style.display="block";
+                                }
+                            
+                                function vid34(){
+                                    document.getElementById("xmen7js").style.display="block";
+                                }
+                    
+                                function vid35(){
+                                    document.getElementById("xmen8js").style.display="block";
+                                }
+                    
+                                function vid36(){
+                                    document.getElementById("xmen9js").style.display="block";
+                                }
+                    
+                                
+                                function vid37(){
+                                    document.getElementById("xmen10js").style.display="block";
+                                }
+                    
+                                function vid38(){
+                                    document.getElementById("xmen11js").style.display="block";
+                                }
+                    
+                                
+                                function vid39(){
+                                    document.getElementById("xmen12js").style.display="block";
+                                }
+                    
+                                function vid40(){
+                                    document.getElementById("xmen13js").style.display="block";
+                                }
+                    
+                                function vid41(){
+                                    document.getElementById("fan1js").style.display="block";
+                                }
+                    
+                                
+                                function vid42(){
+                                    document.getElementById("toby2js").style.display="block";
+                                }
+                    
+                                function vid43(){
+                                    document.getElementById("toby3js").style.display="block";
+                                }
+                    
+                                
+                                function vid44(){
+                                    document.getElementById("toby1js").style.display="block";
+                                }
+                    
+                                function vid45(){
+                                    document.getElementById("toby2js").style.display="block";
+                                }
+                    
+                                function vid46(){
+                                    document.getElementById("toby3js").style.display="block";
+                                }
+                    
+                                
+                                function vid47(){
+                                    document.getElementById("andrew1js").style.display="block";
+                                }
+                    
+                                function vid48(){
+                                    document.getElementById("andrew2js").style.display="block";
+                                }
+                    
+                                function vid49(){
+                                    document.getElementById("galaxy3js").style.display="block";
+                                }
+                    
+                                function vid50(){
+                                    document.getElementById("galaxy03js").style.display="block";
+                                }
+                    
+
+
+
+                                var swiper = new Swiper(".mySwiper", {
+                                    spaceBetween: 30,
+                                    pagination: {
+                                      el: ".swiper-pagination",
+                                      clickable: true,
+                                    },
+
+                                    autoplay: {
+                                        delay: 1500,
+                                        disableOnInteraction: false,
+                                      },
+
+                                  });
